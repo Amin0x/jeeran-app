d56a44dda77021c8f631ac08c467e562621dfd9e
amin_0 <garogigi@gmail.com>
2020-04-23 17:39:22 +0200

    implement main view

--- app/src/main/java/sd/jeeran/ui/adapter/MainContentActivityAdapter.java
+++ app/src/main/java/sd/jeeran/ui/adapter/MainContentActivityAdapter.java
@@ -1,26 +1,74 @@
 package sd.jeeran.ui.adapter;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.zip.Inflater;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import sd.jeeran.R;
+import sd.jeeran.ui.fragment.MainContentFragment;
 
 
-public class MainContentActivityAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class MainContentActivityAdapter extends RecyclerView.Adapter<MainContentActivityAdapter.ItemViewHolder> {
+
+    private List<MainContentFragment.DataListItem> data;
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder{
+        public View view;
+
+        public ItemViewHolder(@NonNull View view) {
+            super(view);
+            this.view = view;
+        }
+    }
+
+    public MainContentActivityAdapter(List<MainContentFragment.DataListItem> data) {
+        this.data = data;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+        int type = 0 /*data.get(viewType).getType()*/;
+        View view = null;
+
+        if (type == 0){
+            view = inflater.inflate(R.layout.main_content_item_0, parent, false);
+        }
+
+        if (type == 1){
+            view = inflater.inflate(R.layout.main_content_item_1, parent, false);
+        }
+
+        if (type == 2){
+            view = inflater.inflate(R.layout.main_content_item_2, parent, false);
+        }
+
+
+        return new ItemViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        MainContentFragment.DataListItem item = data.get(position);
 
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(holder.view.getContext(), "you clicked item" + position + "and item type" + item.type, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 }
--- app/src/main/java/sd/jeeran/ui/fragment/MainContentFragment.java
+++ app/src/main/java/sd/jeeran/ui/fragment/MainContentFragment.java
@@ -4,25 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import sd.jeeran.R;
-import sd.jeeran.ui.main.PageViewModel;
+import sd.jeeran.ui.adapter.MainContentActivityAdapter;
 
 /**
- * A placeholder fragment containing a simple view.
+ *
  */
 public class MainContentFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private List<DataListItem> dataSource = new ArrayList<DataListItem>();
 
+    public class DataListItem {
+        public int type;
 
+        public DataListItem(int i) {
+            type = i;
+        }
+    }
     public static MainContentFragment newInstance(int index) {
         MainContentFragment fragment = new MainContentFragment();
         Bundle bundle = new Bundle();
@@ -34,18 +41,24 @@ public class MainContentFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
     }
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
-        final RecyclerView textView = root.findViewById(R.id.section_label);
 
+        dataSource.add(new DataListItem(0));
+        dataSource.add(new DataListItem(2));
+        dataSource.add(new DataListItem(0));
+        dataSource.add(new DataListItem(1));
+        dataSource.add(new DataListItem(2));
+        dataSource.add(new DataListItem(1));
+        dataSource.add(new DataListItem(0));
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        final RecyclerView recyclerView = root.findViewById(R.id.section_label);
+        recyclerView.setAdapter(new MainContentActivityAdapter(dataSource));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         return root;
     }
 }
