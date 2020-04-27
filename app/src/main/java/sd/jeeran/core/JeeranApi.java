9179f483866da05db4c80b4fc8bf50df8841c02f
amin_0 <garogigi@gmail.com>
2020-04-28 00:03:01 +0200

    create httpApi class implement ProfileActivity fix drawer and create toolbar menu

--- app/src/main/java/sd/jeeran/core/JeeranApi.java
+++ app/src/main/java/sd/jeeran/core/JeeranApi.java
@@ -1,9 +1,26 @@
 package sd.jeeran.core;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface JeeranApi {
+
+
     @GET("api/")
-    public Call<String> getHome();
+    Call<String> getHome();
+
+
+    @GET("api/posts")
+    Call<httpApi.Post> getAllPost();
+
+    @GET("api/posts/:id")
+    Call<httpApi.Post> getPost(@Path("id") long id);
+
+
+    @POST("api/posts")
+    Call<httpApi.Post> createPost(@Body httpApi.Post post);
 }
